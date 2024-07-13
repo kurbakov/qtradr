cf038b30deeaeaf410dede27669800efc72a0936
kurbakov <kurbakov.d@gmail.com>
2024-07-13 16:54:02 +0200

    Fix warning for logger

--- misc/logger/inc/logger/logger.hpp
+++ misc/logger/inc/logger/logger.hpp
@@ -8,8 +8,8 @@
 #include <concurrency/lfqueue/ring/mpmc.hpp>
 
 #include "defs.hpp"
-#include "utils.hpp"
 #include "processor.hpp"
+#include "utils.hpp"
 
 namespace
 {
@@ -30,27 +30,26 @@ template <typename... Args> void LOG_IMPL(const logger::Meta &meta, Args &&...ar
 }
 } // namespace
 
-#define LOG_DEBUG(fmt, ...)                                                                                             \
+#define LOG_DEBUG(fmt, ...)                                                                                            \
     do                                                                                                                 \
     {                                                                                                                  \
-        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                                       \
+        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                       \
         constexpr logger::Meta meta{                                                                                   \
-            .level{logger::Level::DEBUG},                                                                               \
-            .location{std::source_location::current()},                                                                \
-            .pattern{fmt},                                                                                             \
+            .level = logger::Level::DEBUG,                                                                             \
+            .location = std::source_location::current(),                                                               \
+            .pattern = fmt,                                                                                            \
         };                                                                                                             \
         LOG_IMPL(meta, __VA_ARGS__);                                                                                   \
     } while (0)
 
-
 #define LOG_INFO(fmt, ...)                                                                                             \
     do                                                                                                                 \
     {                                                                                                                  \
-        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                                       \
+        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                       \
         constexpr logger::Meta meta{                                                                                   \
-            .level{logger::Level::INFO},                                                                               \
-            .location{std::source_location::current()},                                                                \
-            .pattern{fmt},                                                                                             \
+            .level = logger::Level::INFO,                                                                              \
+            .location = std::source_location::current(),                                                               \
+            .pattern = fmt,                                                                                            \
         };                                                                                                             \
         LOG_IMPL(meta, __VA_ARGS__);                                                                                   \
     } while (0)
@@ -58,11 +57,11 @@ template <typename... Args> void LOG_IMPL(const logger::Meta &meta, Args &&...ar
 #define LOG_WARN(fmt, ...)                                                                                             \
     do                                                                                                                 \
     {                                                                                                                  \
-        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                                       \
+        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                       \
         constexpr logger::Meta meta{                                                                                   \
-            .level{logger::Level::WARN},                                                                               \
-            .location{std::source_location::current()},                                                                \
-            .pattern{fmt},                                                                                             \
+            .level = logger::Level::INFO,                                                                              \
+            .location = std::source_location::current(),                                                               \
+            .pattern = fmt,                                                                                            \
         };                                                                                                             \
         LOG_IMPL(meta, __VA_ARGS__);                                                                                   \
     } while (0)
@@ -70,11 +69,11 @@ template <typename... Args> void LOG_IMPL(const logger::Meta &meta, Args &&...ar
 #define LOG_ERROR(fmt, ...)                                                                                            \
     do                                                                                                                 \
     {                                                                                                                  \
-        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                                       \
+        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                       \
         constexpr logger::Meta meta{                                                                                   \
-            .level{logger::Level::ERROR},                                                                               \
-            .location{std::source_location::current()},                                                                \
-            .pattern{fmt},                                                                                             \
+            .level = logger::Level::ERROR,                                                                             \
+            .location = std::source_location::current(),                                                               \
+            .pattern = fmt,                                                                                            \
         };                                                                                                             \
         LOG_IMPL(meta, __VA_ARGS__);                                                                                   \
     } while (0)
@@ -82,11 +81,11 @@ template <typename... Args> void LOG_IMPL(const logger::Meta &meta, Args &&...ar
 #define LOG_FATAL(fmt, ...)                                                                                            \
     do                                                                                                                 \
     {                                                                                                                  \
-        static_assert(logger::placeholders_count(fmt, "{}") == args_count(__VA_ARGS__));                                       \
+        static_assert(logger::placeholders_count(fmt, "{}") == logger::args_count(__VA_ARGS__));                       \
         constexpr logger::Meta meta{                                                                                   \
-            .level{logger::Level::FATAL},                                                                               \
-            .location{std::source_location::current()},                                                                \
-            .pattern{fmt},                                                                                             \
+            .level = logger::Level::FATAL,                                                                             \
+            .location = std::source_location::current(),                                                               \
+            .pattern = fmt,                                                                                            \
         };                                                                                                             \
         LOG_IMPL(meta, __VA_ARGS__);                                                                                   \
     } while (0)
--- misc/logger/test/unit/logger.ut.cpp
+++ misc/logger/test/unit/logger.ut.cpp
@@ -14,4 +14,8 @@ TEST(Logger, LogData) {
     logger::Processor<FakeQueue>::init(logger::Level::INFO);
 
     LOG_INFO("hello {} {}", 0, 1);
+    LOG_WARN("hello {} {}", 0, 1);
+    LOG_DEBUG("hello {} {}", 0, 1);
+    LOG_ERROR("hello {} {}", 0, 1);
+    LOG_FATAL("hello {} {}", 0, 1);
 }
